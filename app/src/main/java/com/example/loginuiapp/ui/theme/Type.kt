21417862d2858ccc9f2e73823cd1517207f2d28e
MohammadNikmard <mnikmard2004@gmail.com>
2024-08-30 11:07:45 +0330

    feature: add essential things

--- app/src/main/java/com/example/loginuiapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/loginuiapp/ui/theme/Type.kt
@@ -2,11 +2,12 @@ package com.example.loginuiapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.loginuiapp.R
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -15,20 +16,20 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+)
+
+val poppinsBold = FontFamily(
+    Font(R.font.poppins_bold)
+)
+
+val poppinsMedium = FontFamily(
+    Font(R.font.poppins_medium)
+)
+
+val poppinsRegular = FontFamily(
+    Font(R.font.poppins_regular)
+)
+
+val poppinsSemiBold = FontFamily(
+    Font(R.font.poppins_semibold)
 )
