a7b0030058a0181404907dfd5831558ed1ad6d63
MohammadNikmard <mnikmard2004@gmail.com>
2024-09-01 20:43:51 +0330

    chore: fix back layer shapes

--- app/src/main/java/com/example/loginuiapp/backLayerDesign.kt
+++ app/src/main/java/com/example/loginuiapp/backLayerDesign.kt
@@ -1,20 +1,31 @@
 package com.example.loginuiapp
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun BackLayerDesign(){
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopStart) {
+fun BackLayerDesign() {
+    Box(contentAlignment = Alignment.TopStart) {
+
+
         Canvas(
             modifier = Modifier
                 .size(635.dp)
@@ -25,9 +36,45 @@ fun BackLayerDesign(){
         ) {
             drawCircle(
                 color = Color(0xffF8F9FF),
+
                 radius = size.minDimension / 2,
+
+                )
+        }
+
+        Canvas(
+            modifier = Modifier
+                .offset(
+                    x = (280).dp,
+                    y = (80).dp
+                )
+        ) {
+            drawCircle(
+                color = Color(0xffF8F9FF),
+                radius = 650f,
+                style = Stroke(
+                    width = 6f,
+                )
             )
         }
+
+        Canvas(
+            modifier = Modifier
+                .offset(
+                    x = (-50.0).dp,
+                )
+                .rotate(30f)
+                .align(Alignment.BottomStart)
+        ) {
+            drawRect(
+                color = Color(0xffF8F9FF),
+                size = Size(height = 650f, width = 650f),
+                style = Stroke(
+                    width =10f,
+                )
+            )
+        }
+
         Canvas(
             modifier = Modifier
                 .size(372.dp)
@@ -38,6 +85,5 @@ fun BackLayerDesign(){
                 .border(width = 3.dp, color = Color(0xffF8F9FF)),
         ) {
         }
-
     }
 }
